78b0f58544cdf70b48da33807b69c9ce3322b9a4
gitstabares <stabares@unal.edu.co>
2026-09-19 16:13:34 -0500

    Fix: syntax corrections in tree and node

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from pydoc import text
+from nicegui import ui
+from src.model import *
+from src.views import get_echart_dict
 
-import streamlit as st
-import src.model as m
-
-tree = m.Tree()
-for i in range(6,0,-2):
+tree = Tree()
+for i in range(6,0,-1):
     tree.add_node(i)
-for i in range(7,0,-2):
-    tree.add_node(i)
-st.graphviz_chart(tree.get_tree_graph())
+ui.label('Grafo')
 tree.balance_tree()
-st.graphviz_chart(tree.get_tree_graph())
-print(tree.get_weight())
+echarts = get_echart_dict(tree)
+ui.echart(echarts).classes('w-full h-screen')
+ui.run(dark=True, language='es')
